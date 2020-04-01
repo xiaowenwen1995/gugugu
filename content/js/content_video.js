edaf3ed044e12edc9b70328a8046ccd20faabb99
weiwenwen1995 <cuteaghost@163.com>
2020-04-01 21:46:39 +0800

    修复jquery的bug

--- content/js/content_video.js
+++ content/js/content_video.js
@@ -31,33 +31,6 @@ function init(){
 
 window.setTimeout(init,2000);
 
-
-jQuery.fn.wait = function (func, times, interval) {
-    var _times = times || -1, //100次
-    _interval = interval || 20, //20毫秒每次
-    _self = this,
-    _selector = this.selector, //选择器
-    _iIntervalID; //定时器id
-    if( this.length ){ //如果已经获取到了，就直接执行函数
-        func && func.call(this);
-    } else {
-        _iIntervalID = setInterval(function() {
-            if(!_times) { //是0就退出
-                clearInterval(_iIntervalID);
-            }
-            _times <= 0 || _times--; //如果是正数就 --
-             
-            _self = $(_selector); //再次选择
-            if( _self.length ) { //判断是否取到
-                func && func.call(_self);
-                clearInterval(_iIntervalID);
-            }
-        }, _interval);
-    }
-    return this;
-}
-
-
 //一键三连操作
 function gugu(){
 	//var like = document.getElementsByClassName("like")[0]; //点赞
